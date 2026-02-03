d871a4a06abd0efaf645de91a7324c3b1ee9ba6a
Boya Ajay varma <imav202@gmail.com>
2026-02-03 13:17:36 +0530

    feat: add username search with dynamic API fetching

--- src/GithubUser.jsx
+++ src/GithubUser.jsx
@@ -1,16 +1,18 @@
 import React,{useState,useEffect} from "react";
 function GithubUser(){
+    const[userName,setUserName]=useState("octocat");
     const [user,setUser]=useState(null);
     const[loading,setLoading]=useState(false);
     const[error,setError]=useState(null);
 
     useEffect(()=>{
+        if(!userName)return;
         setLoading(true);
         setError(null);
-        fetch("https://api.github.com/users/octocat")
+        fetch("https://api.github.com/users/${userName}")
         .then((res)=>{
             if(!res.ok){
-                throw new Error("failed to fetch user")
+                throw new Error("user not found")
             }
             return res.json();
         })
@@ -20,12 +22,22 @@ function GithubUser(){
         })
         .catch((err)=>{
             setError(err.message);
+            setUser(null);
             setLoading(false);
         });
-    },[]);
+    },[userName]);
     return (
     <div style={{ padding: "20px" }}>
       <h1>GitHub User</h1>
+      <input
+        type="text"
+        placeholder="Enter Github username"
+        value={userName}
+        onChange={(e)=>setUserName(e.target.value)}
+        style={{padding:"8px",marginBottom:"10px"}}
+      
+      
+      />
 
       {loading && <p>Loading...</p>}
       {error && <p style={{ color: "red" }}>Error: {error}</p>}
