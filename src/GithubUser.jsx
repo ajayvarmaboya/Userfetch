b5f3521de5163693b7da0273242d0a30d7ac5ef9
Boya Ajay varma <imav202@gmail.com>
2026-02-05 16:18:16 +0530

    fix:correctly parse JSON response in user fetch

--- src/GithubUser.jsx
+++ src/GithubUser.jsx
@@ -1,86 +1,78 @@
 import React,{useState,useEffect} from "react";
 function GithubUser(){
-    const[searchUser,setSearchUser]=useState("")
-    const[userName,setUserName]=useState("");
-    const [user,setUser]=useState(null);
-    const[loading,setLoading]=useState(false);
-    const[error,setError]=useState(null);
-    useEffect(()=>{
-      if(!userName){
-        setUser(null);
-        setError(null);
-      }
-    },[userName])
-    useEffect(()=>{
-      const timeout=setTimeout(()=>{
-        setSearchUser(userName);
-      },500);
-      return()=>{
-        clearTimeout(timeout);
-      }
-    },[userName]);
+  const[input,setInput]=useState("");
+  const[debouncedInput,setDebouncedInput]=useState("");
+  const[user,setUser]=useState(null);
+  const[loading,setLoading]=useState(false);
+  const[error,setError]=useState(null);
 
-    useEffect(()=>{
+useEffect(()=>{
+  const timer=setTimeout(()=>{
+    setDebouncedInput(input);
+  },500);
+  return()=> clearTimeout(timer);
+},[input]);
 
-        if(!searchUser)return;
-        const controller=new AbortController();
-        const signal=controller.signal;
-        setLoading(true);
-        setError(null);
-        console.log("Fetching user:", searchUser);
-        fetch(`https://jsonplaceholder.typicode.com/users/${searchUser}`, {
-  signal
-})
-        .then((res)=>{
-            if(!res.ok){
-                throw new Error("user not found")
-            }
-            return res.json();
-        })
-        .then((data)=>{
-            setUser(data);
-            setLoading(false);
-        })
-        .catch((err)=>{
-            if(err.name ==="AbortError")return;
-            setError(err.message);
-            setUser(null);
-            setLoading(false);
-        });
-      return()=>{
-        controller.abort();
+
+useEffect(()=>{
+  if(!debouncedInput){
+    setUser(null);
+    setError(null);
+    return;
+  }
+  const controller=new AbortController();
+  const signal=controller.signal;
+
+  setLoading(true);
+  setError(null);
+  fetch(`https://jsonplaceholder.typicode.com/users/${debouncedInput}`, {
+      signal,
+    })
+    .then((res)=>{
+      if(!res.ok){
+        throw new Error ("user not found");
       }
-    },[searchUser]);
-    return (
-    <div style={{ padding: "20px" }}>
-      <h1>GitHub User</h1>
-      <input
-        type="text"
-        placeholder="Enter Github username"
-        value={userName}
-        onChange={(e)=>setUserName(e.target.value)}
-        style={{padding:"8px",marginBottom:"10px"}}
-      />
-      
+      return res.json();
+    })
+    .then((data)=>{
+      setUser(data);
+      setLoading(false);
+    })
+    .catch((err)=>{
+      if(err.name==="AbortError")return;
+      setError(err.message);
+      setUser(null);
+      setLoading(false);
+    });
+    return()=>controller.abort();
 
-      {!loading && error &&(
-        <p style={{ color:"red"}}>
-          Error:{error}
-        </p>
-      )}
-      {user && (
-        <div>
-          <h2>{user.name}</h2>
-          <img src={user.avatar_url} alt="avatar" width="120" />
-          <p>{user.bio}</p>
-          <p><strong>Name:</strong> {user.name}</p>
-          <p><strong>Email:</strong> {user.email}</p>
-          <p><strong>Company:</strong> {user.company.name}</p>
-        </div>
-      )}
-    </div>
-  );
+
+},[debouncedInput]);
+return(
+  <div style={{padding:"20px"}}>
+    <h1> User Fetch</h1>
+  
+
+  <input 
+    type="text"
+    placeholder="Enter the user is(1-10)"
+    value={input}
+    onChange={(e)=>setInput(e.target.value)}
+    style={{padding:"8px",marginBottom:"10px"}}
+    />
+    {loading &&<p>Loading...</p>}
+    {!loading && error &&(
+      <p style ={{color:"red"}}>Error:{error}</p>
+    )}
+    {user &&(
+      <div>
+        <p><strong>Name:</strong>{user.name}</p>
+        <p><strong>Email:</strong>{user.email}</p>
+        <p><strong>Company:</strong>{user.company.name}</p>
+      </div>
+    )}
+  </div>
+
+);
 }
-
 export default GithubUser;
-
